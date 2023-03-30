64b4b47a0100d3c769cce336bedfc3df1b89a94d
saratarekabbas <saraatarekk@icloud.com>
2023-03-31 02:27:17 +0800

    Added logic to login

--- esteshari/resources/views/layouts/old_app.blade.php
+++ esteshari/resources/views/layouts/old_app.blade.php
@@ -22,6 +22,8 @@
 
 </head>
 <body>
+
+
 <div id="app">
     <section class="background-radial-gradient overflow-hidden">
         <div class="container px-4 py-5 px-md-5 text-center text-lg-start my-5">
@@ -70,10 +72,69 @@
 
                             <!-- Pills content -->
                             <div class="tab-content">
-                                @yield('content')
-{{--                                LOGIN--}}
+                                <div class="tab-pane fade show active" id="pills-login" role="tabpanel"
+                                     aria-labelledby="tab-login">
+                                    <form>
+                                        <div class="text-center mb-3">
+                                            <p>Sign in with:</p>
+                                            <button type="button" class="btn btn-link btn-floating mx-1">
+                                                <i class="fab fa-facebook-f"></i>
+                                            </button>
 
-{{--                                REGISTER--}}
+                                            <button type="button" class="btn btn-link btn-floating mx-1">
+                                                <i class="fab fa-google"></i>
+                                            </button>
+
+                                            <button type="button" class="btn btn-link btn-floating mx-1">
+                                                <i class="fab fa-twitter"></i>
+                                            </button>
+
+                                            <button type="button" class="btn btn-link btn-floating mx-1">
+                                                <i class="fab fa-github"></i>
+                                            </button>
+                                        </div>
+
+                                        <p class="text-center">or:</p>
+
+                                        <!-- Email input -->
+                                        <div class="form-outline mb-4">
+                                            <input type="email" id="loginName" class="form-control"/>
+                                            <label class="form-label" for="loginName">Email or username</label>
+                                        </div>
+
+                                        <!-- Password input -->
+                                        <div class="form-outline mb-4">
+                                            <input type="password" id="loginPassword" class="form-control"/>
+                                            <label class="form-label" for="loginPassword">Password</label>
+                                        </div>
+
+                                        <!-- 2 column grid layout -->
+                                        <div class="row mb-4">
+                                            <div class="col-md-6 d-flex justify-content-center">
+                                                <!-- Checkbox -->
+                                                <div class="form-check mb-3 mb-md-0">
+                                                    <input class="form-check-input" type="checkbox" value=""
+                                                           id="loginCheck" checked/>
+                                                    <label class="form-check-label" for="loginCheck"> Remember
+                                                        me </label>
+                                                </div>
+                                            </div>
+
+                                            <div class="col-md-6 d-flex justify-content-center">
+                                                <!-- Simple link -->
+                                                <a href="#!">Forgot password?</a>
+                                            </div>
+                                        </div>
+
+                                        <!-- Submit button -->
+                                        <button type="submit" class="btn btn-primary btn-block mb-4">Sign in</button>
+
+                                        <!-- Register buttons -->
+                                        <div class="text-center">
+                                            <p>Not a member? <a href="#!">Register</a></p>
+                                        </div>
+                                    </form>
+                                </div>
                                 <div class="tab-pane fade" id="pills-register" role="tabpanel"
                                      aria-labelledby="tab-register">
                                     <form>
