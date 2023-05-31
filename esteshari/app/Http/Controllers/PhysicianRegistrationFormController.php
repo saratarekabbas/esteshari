af7f4797ee203cca23888f91ede9682a0920ffe3
saratarekabbas <saraatarekk@icloud.com>
2023-05-31 23:13:35 +0800

    Revert "Minimized section 2 size (for now) to make it dynamic."
    This reverts commit b947c8b06fea5170626b2602d4e368155a782416.

--- esteshari/app/Http/Controllers/PhysicianRegistrationFormController.php
+++ esteshari/app/Http/Controllers/PhysicianRegistrationFormController.php
@@ -350,26 +350,101 @@ class PhysicianRegistrationFormController extends Controller
         $user = Auth::user();
         $user->status = 'pending';
         $user->save();
-
-
-        //        SINGLE FILE
-//        if ($request->hasFile('passport')) {
-//            $passportFile = $request->file('passport');
-//            $passportFilePath = $passportFile->store('files'); // Store the passport file in the 'files' directory
-//            // Save the passport file path to the database
-//            $physicianRegistration->passport_file = $passportFilePath;
-//        }
-//        //        MULTIPLE FILES
-//        if ($request->hasFile('insurance')) {
-//            $insuranceFiles = [];
-//            foreach ($request->file('insurance') as $insuranceFile) {
-//                $insuranceFilePath = $insuranceFile->store('files'); // Store each insurance file in the 'files' directory
-//                $insuranceFiles[] = $insuranceFilePath;
-//            }
-//            // Save the insurance files' paths to the database as a JSON array
-//            $physicianRegistration->insurance_files = json_encode($insuranceFiles);
-//        }
-
     }
 
+    /*  public function store(Request $request)
+      {
+          // Validate the form data
+  //        $validatedData = $request->validate([
+  //            'full_name' => 'required|string',
+  //            'phone_number' => 'required',
+  //            'job_title' => 'required',
+  //        ]);
+
+          // Create a new PhysicianRegistration instance
+          $physicianRegistration = new PhysicianRegistration();
+
+          // Set the values from the form
+
+
+  //        SECTION 1: PERSONAL INFO
+          // Check if the selected title is "Other"
+          if ($request->input('title') === 'Other') {
+              // Save the value from the "Other Title" input field
+              $physicianRegistration->title = $request->input('otherTitle');
+          } else {
+              // Save the selected title
+              $physicianRegistration->title = $request->input('title');
+          }
+
+          $physicianRegistration->full_name = $request->input('full_name');
+          $physicianRegistration->date_of_birth = $request->input('date_of_birth');
+          $physicianRegistration->gender = $request->input('gender');
+          $physicianRegistration->alternative_email_address = $request->input('alternative_email_address');
+          $physicianRegistration->nationality = $request->input('nationality');
+          $physicianRegistration->country_code = $request->input('country_code');
+          $physicianRegistration->mobile_number = $request->input('mobile_number');
+          $physicianRegistration->telephone_number = $request->input('telephone_number');
+          $physicianRegistration->street_address = $request->input('street_address');
+          $physicianRegistration->street_address2 = $request->input('street_address2');
+          $physicianRegistration->city = $request->input('city');
+          $physicianRegistration->state_province = $request->input('state_province');
+          $physicianRegistration->postal_code = $request->input('postal_code');
+          $physicianRegistration->country = $request->input('country');
+          if ($request->hasFile('identity_verification_files')) {
+              $identityVerificationFiles = [];
+              foreach ($request->file('identity_verification_files') as $identityVerificationFile) {
+                  $insuranceFilePath = $identityVerificationFile->store('files'); // Store each insurance file in the 'files' directory
+                  $identityVerificationFiles[] = $insuranceFilePath;
+              }
+              // Save the insurance files' paths to the database as a JSON array
+              $physicianRegistration->identity_verification_files = json_encode($identityVerificationFiles);
+          }
+
+
+  //        SINGLE FILE
+          if ($request->hasFile('passport')) {
+              $passportFile = $request->file('passport');
+              $passportFilePath = $passportFile->store('files'); // Store the passport file in the 'files' directory
+              // Save the passport file path to the database
+              $physicianRegistration->passport_file = $passportFilePath;
+          }
+  //        MULTIPLE FILES
+          if ($request->hasFile('insurance')) {
+              $insuranceFiles = [];
+              foreach ($request->file('insurance') as $insuranceFile) {
+                  $insuranceFilePath = $insuranceFile->store('files'); // Store each insurance file in the 'files' directory
+                  $insuranceFiles[] = $insuranceFilePath;
+              }
+              // Save the insurance files' paths to the database as a JSON array
+              $physicianRegistration->insurance_files = json_encode($insuranceFiles);
+          }
+
+
+          $user = Auth::user();
+
+          // Check if the "same email" checkbox is selected
+          if ($request->has('same_email')) {
+              // Use the same email address as the user's registration
+              $email = $user->email;
+          } else {
+              // Use the email address entered in the form
+              $email = $request->input('email');
+          }
+
+          // Set the email address for the physician registration
+          $physicianRegistration->email = $email;
+          $physicianRegistration->user()->associate($user);
+
+          // Save the physician registration data
+          $physicianRegistration->save();
+
+          // Update the user's status to 'pending'
+          $user->status = 'pending';
+          $user->save();
+
+  //            return back()->with('success', 'Registration submitted successfully!');
+          return redirect()->route('physician.pending')->with('success', 'Registration submitted successfully!');
+      }
+    */
 }
