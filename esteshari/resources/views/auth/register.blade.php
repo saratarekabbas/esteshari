379609ef8927b76c10d505e52bbd81b550326318
saratarekabbas <saraatarekk@icloud.com>
2023-06-07 18:04:29 +0800

    registration is in place

--- esteshari/resources/views/auth/register.blade.php
+++ esteshari/resources/views/auth/register.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-    <form>
+    <form method="POST" action="{{ route('register') }}">
+        @csrf
         <div class="text-center mb-3">
             <p>Sign up with:</p>
             <button type="button" class="btn btn-link btn-floating mx-1">
@@ -23,37 +24,64 @@
 
         <p class="text-center">or:</p>
 
-        <!-- Name input -->
         <div class="form-outline mb-4">
-            <input type="text" id="registerName" class="form-control"/>
-            <label class="form-label" for="registerName">Name</label>
+            <input type="text" id="name" class="form-control @error('name') is-invalid @enderror" name="name"
+                   value="{{ old('name') }}" required autocomplete="name" autofocus>
+            <label class="form-label" for="name">{{ __('Name') }}</label>
+
+            @error('name')
+            <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+            @enderror
         </div>
 
-        <!-- Username input -->
         <div class="form-outline mb-4">
-            <input type="text" id="registerUsername" class="form-control"/>
-            <label class="form-label" for="registerUsername">Username</label>
+            <select id="role" class="form-select @error('role') is-invalid @enderror"
+                    name="role" value="{{ old('role') }}" required autocomplete="role">
+                <option value="patient">Patient</option>
+                <option value="physician">Physician</option>
+            </select>
+
+            @error('role')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+            @enderror
+
+            <label for="role" class="form-label">{{ __('Role') }}</label>
         </div>
 
-        <!-- Email input -->
+
         <div class="form-outline mb-4">
-            <input type="email" id="registerEmail" class="form-control"/>
-            <label class="form-label" for="registerEmail">Email</label>
+            <input type="email" id="email" class="form-control @error('email') is-invalid @enderror" name="email"
+                   value="{{ old('email') }}" required autocomplete="email">
+            @error('email')
+            <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+            @enderror
+
+            <label class="form-label" for="email">{{ __('Email Address') }}</label>
+
         </div>
 
-        <!-- Password input -->
         <div class="form-outline mb-4">
-            <input type="password" id="registerPassword" class="form-control"/>
-            <label class="form-label" for="registerPassword">Password</label>
+            <input type="password" id="password" class="form-control @error('password') is-invalid @enderror"
+                   name="password"
+                   required autocomplete="new-password">
+            <label class="form-label" for="password">{{ __('Password') }}</label>
         </div>
 
-        <!-- Repeat Password input -->
         <div class="form-outline mb-4">
-            <input type="password" id="registerRepeatPassword" class="form-control"/>
-            <label class="form-label" for="registerRepeatPassword">Confirm
-                password</label>
+            <input type="password" id="password-confirm" class="form-control "
+                   name="password_confirmation" required autocomplete="new-password">
+            <label class="form-label" for="password-confirm">{{ __('Confirm Password') }}</label>
         </div>
-        <!-- Submit button -->
-        <button type="submit" class="btn btn-primary btn-block mb-3">Sign in</button>
+        <button type="submit" class="btn btn-primary btn-block mb-3">{{ __('Register') }}</button>
+        <div class="text-center">
+            <p>Already a member? <a href="{{ route('login') }}">Login</a></p>
+        </div>
+
     </form>
 @endsection
